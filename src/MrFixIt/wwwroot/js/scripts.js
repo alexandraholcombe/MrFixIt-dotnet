345babc74a5f18c7f91afcfc71e1d50f3e55f91c
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-05-05 10:46:16 -0700

    Fetch claim confirmation partial with async

--- src/MrFixIt/wwwroot/js/scripts.js
+++ src/MrFixIt/wwwroot/js/scripts.js
@@ -1,1 +1,17 @@
 ﻿var test = "it works";
+
+$(document).ready(function () {
+    $(".claim-job").click(function () {
+        //debugger;
+        var currentId = $(this).parent().attr('id');
+        console.log(currentId);
+        $.ajax({
+            type: 'GET',
+            dataType: 'html',
+            url: $(this).data('request-url'),
+            success: function (result) {
+                $('.unclaimed').html(result);
+            }
+        });
+    });
+});
